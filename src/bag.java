6f85090323ac344fee3cb07e4294c890f1b98f0b
Artarazaz <116476239+Artarazaz@users.noreply.github.com>
2024-05-07 16:37:05 +0330

    just started

---
+++ src/bag.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class bag {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your bag size: ");
+        int bagSize = scanner.nextInt();
+        System.out.println("How many items you want to add?");
+        int items = scanner.nextInt();
+
+    }
+}
