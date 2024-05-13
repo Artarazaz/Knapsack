598f0fa2b42728a9f17575e1c4d25eb9ea92fe85
Artarazaz <116476239+Artarazaz@users.noreply.github.com>
2024-05-13 14:00:54 +0330

    method for store items and values add

--- src/bag.java
+++ src/bag.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class bag {
@@ -7,6 +8,15 @@ public class bag {
         int bagSize = scanner.nextInt();
         System.out.println("How many items you want to add?");
         int items = scanner.nextInt();
-
+        String [] itemsArr = new String[items];
+        int[] valueArr = new int[items];
+        for (int i = 0; i < items; i ++){
+            System.out.println("Please enter your items no." + (i + 1));
+            itemsArr [i] = scanner.next();
+        }
+        for (int i = 0; i < items; i ++){
+            System.out.println("Please enter a value for " + itemsArr[i]);
+            valueArr [i] = scanner.nextInt();
+        }
     }
 }
