15e4920dc10ae1a9935ab7d5273533fc3f92ee99
Artarazaz <116476239+Artarazaz@users.noreply.github.com>
2024-05-26 01:07:48 +0330

    greedy algorithm changed to dynamic algorithm

--- src/bag.java
+++ src/bag.java
@@ -1,77 +1,45 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class bag {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your bag size: ");
-        int bagSize = scanner.nextInt();
-        System.out.println("How many items do you have?");
-        int items = scanner.nextInt();
-        String[] itemsArr = new String[items];
-        int[] valueArr = new int[items];
-        int[] weightArr = new int[items];
-        for (int i = 0; i < items; i++) {
-            System.out.println("Please enter items no. " + (i + 1));
-            itemsArr[i] = scanner.next();
+        System.out.print("Enter the number of items: ");
+        int n = scanner.nextInt();
+        int[] values = new int[n];
+        int[] weights = new int[n];
+
+        System.out.println("Enter the values and weights of each item:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Value of item " + (i + 1) + ": ");
+            values[i] = scanner.nextInt();
+            System.out.print("Weight of item " + (i + 1) + ": ");
+            weights[i] = scanner.nextInt();
         }
 
-        for (int i = 0; i < items; i++) {
-            System.out.println("Please enter a value and weight for " + itemsArr[i]);
-            valueArr[i] = scanner.nextInt();
-            weightArr[i] = scanner.nextInt();
-        }
-        double[] valueToWeight = new double[items];
-        for (int i = 0; i < valueArr.length; i++) {
-            valueToWeight[i] = (double) valueArr[i] / weightArr[i];
-        }
-        for (int j = 0; j < valueArr.length; j++) {
-            for (int i = 0; i < valueArr.length - 1; i++) {
-                if (valueToWeight[i] < valueToWeight[i + 1]) {
-                    int saver = valueArr[i];
-                    valueArr[i] = valueArr[i + 1];
-                    valueArr[i + 1] = saver;
-                    int saver2 = weightArr[i];
-                    weightArr[i] = weightArr[i + 1];
-                    weightArr[i + 1] = saver2;
-                    double saver1 = valueToWeight[i];
-                    valueToWeight[i] = valueToWeight[i + 1];
-                    valueToWeight[i + 1] = saver1;
-                    String string = itemsArr[i];
-                    itemsArr[i] = itemsArr[i + 1];
-                    itemsArr[i + 1] = string;
+        System.out.print("Enter the capacity of the knapsack: ");
+        int capacity = scanner.nextInt();
+
+        int[][] valueTable = new int[n + 1][capacity + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= capacity; j++) {
+                if (weights[i - 1] <= j) {
+                    valueTable[i][j] = Math.max(valueTable[i - 1][j], values[i - 1] + valueTable[i - 1][j - weights[i - 1]]);
+                } else {
+                    valueTable[i][j] = valueTable[i - 1][j];
                 }
             }
         }
-        int i = 0;
-        int itemsCounter = 0;
-        List<String> chosenItem = new ArrayList<>();
-        List<Integer> chosenItemCounter = new ArrayList<>();
-        while (bagSize != 0) {
-            if (bagSize >= weightArr[i]) {
-                itemsCounter++;
-                bagSize = bagSize - weightArr[i];
-                if (!chosenItem.contains(itemsArr[i])) {
-                    chosenItem.add(itemsArr[i]);
-                }
+
+        System.out.println("Maximum value that can be carried: " + valueTable[n][capacity]);
+
+        int w = capacity;
+        System.out.print("Selected items: ");
+        for (int i = n; i > 0 && w > 0; i--) {
+            if (valueTable[i][w] != valueTable[i - 1][w]) {
+                System.out.print((i) + " ");
+                w -= weights[i - 1];
             }
-            if (bagSize < weightArr[i]) {
-                if (itemsCounter != 0) {
-                    chosenItemCounter.add(itemsCounter);
-                }
-                i++;
-                itemsCounter = 0;
-            }
-            if (i == weightArr.length) {
-                break;
-            }
-        }
-        System.out.println("The perfect number of items for fill the bag with");
-        System.out.println("highest value and lowest weight:");
-        for (int j = 0; j < chosenItem.size(); j++) {
-            System.out.println("   " + chosenItemCounter.get(j)
-                    + " of " + chosenItem.get(j));
         }
     }
 }
